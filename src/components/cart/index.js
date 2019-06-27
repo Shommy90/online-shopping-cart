2f5ca0d904850524886f48d15ec7699014e6b043
Milovan Marjanovic <shommy@Milovans-MacBook-Pro.local>
2019-06-27 17:01:51 +0200

    comment out cartItemsWithQuantity

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 
-export const cartItemsWithQuantity = cartItems => {
-  return cartItems.reduce((acc, item) => {
-    const filteredItem = acc.filter(item2 => item2.id === item.id)[0];
-    filteredItem !== undefined
-      ? filteredItem.quantity++
-      : acc.push({ ...item, quantity: 1 });
+// export const cartItemsWithQuantity = cartItems => {
+//   return cartItems.reduce((acc, item) => {
+//     const filteredItem = acc.filter(item2 => item2.id === item.id)[0];
+//     filteredItem !== undefined
+//       ? filteredItem.quantity++
+//       : acc.push({ ...item, quantity: 1 });
 
-    return acc;
-  }, []);
-};
+//     return acc;
+//   }, []);
+// };
 
 function sort(items) {
   return items.sort((a, b) => a.id - b.id);
--- src/components/products-listing/index.js
+++ src/components/products-listing/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ProductListItem from "./product-list-item";
 import { connect } from "react-redux";
-import { cartItemsWithQuantity } from "../cart";
+// import { cartItemsWithQuantity } from "../cart";
 
 function ProductListing(props) {
   console.log(props);
